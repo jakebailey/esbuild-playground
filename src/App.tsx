808b17f9dce9ad1cae9f12865154dc7a0ff92295
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-02-05 14:39:10 -0800

    Refactor root

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { javascript } from "@codemirror/lang-javascript";
-import { Container, SimpleGrid, useMantineTheme } from "@mantine/core";
+import { ActionIcon, Container, Group, SimpleGrid, Text, useMantineColorScheme } from "@mantine/core";
 import { useDebouncedValue, useHash } from "@mantine/hooks";
+import { IconMoonStars, IconSun } from "@tabler/icons-react";
 import CodeMirror from "@uiw/react-codemirror";
+import { version } from "esbuild-wasm/package.json";
 import { useMemo } from "react";
 
 import { useEsbuild } from "./esbuild";
@@ -39,7 +41,8 @@ module.exports = (message) => {
 `.trim();
 
 export function App() {
-    const theme = useMantineTheme();
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
     const [hash, setHash] = useHash();
     const value = useMemo(() => {
@@ -57,35 +60,55 @@ export function App() {
     const built = useEsbuild(debouncedValue);
 
     return (
-        <Container fluid p={0}>
-            <SimpleGrid
-                cols={2}
-                p={0}
-                spacing={0}
-                breakpoints={[
-                    { minWidth: 800, cols: 2 },
-                    { maxWidth: 800, cols: 1, spacing: "sm" },
-                ]}
-            >
-                <CodeMirror
-                    theme={theme.colorScheme}
-                    autoFocus
-                    height="100vh"
-                    value={value}
-                    onChange={(value) => setHash("#" + btoa(value))}
-                    extensions={[javascript({
-                        jsx: true,
-                        typescript: true,
-                    })]}
-                />
-                <CodeMirror
-                    theme={theme.colorScheme}
-                    readOnly
-                    height="100vh"
-                    value={built}
-                    extensions={[javascript()]}
-                />
-            </SimpleGrid>
-        </Container>
+        <>
+            <div style={{ position: "absolute", top: 0, right: 0, margin: "1rem", zIndex: 1 }}>
+                <Group>
+                    <Text component="a" href="https://esbuild.github.io/" target="_blank">
+                        esbuild v{version}
+                    </Text>
+                    <ActionIcon
+                        variant="outline"
+                        onClick={() => toggleColorScheme()}
+                        size="lg"
+                        sx={(theme) => ({
+                            backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+                            color: theme.colorScheme === "dark" ? theme.colors.yellow[4] : theme.colors.blue[6],
+                        })}
+                    >
+                        {colorScheme === "dark" ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+                    </ActionIcon>
+                </Group>
+            </div>
+            <Container fluid p={0}>
+                <SimpleGrid
+                    cols={2}
+                    p={0}
+                    spacing={0}
+                    breakpoints={[
+                        { minWidth: 800, cols: 2 },
+                        { maxWidth: 800, cols: 1, spacing: "sm" },
+                    ]}
+                >
+                    <CodeMirror
+                        theme={colorScheme}
+                        autoFocus
+                        height="100vh"
+                        value={value}
+                        onChange={(value) => setHash("#" + btoa(value))}
+                        extensions={[javascript({
+                            jsx: true,
+                            typescript: true,
+                        })]}
+                    />
+                    <CodeMirror
+                        theme={colorScheme}
+                        readOnly
+                        height="100vh"
+                        value={built}
+                        extensions={[javascript()]}
+                    />
+                </SimpleGrid>
+            </Container>
+        </>
     );
 }
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,5 @@
-import { ActionIcon, ColorScheme, Group, MantineProvider, Text } from "@mantine/core";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
 import { useColorScheme, useLocalStorage } from "@mantine/hooks";
-import { IconMoonStars, IconSun } from "@tabler/icons-react";
-import { version } from "esbuild-wasm/package.json";
 
 import { App } from "./App";
 
@@ -16,30 +14,14 @@ export function Root() {
         setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
     return (
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{ colorScheme }}
-        >
-            <div style={{ position: "absolute", top: 0, right: 0, margin: "1rem", zIndex: 1 }}>
-                <Group>
-                    <Text component="a" href="https://esbuild.github.io/" target="_blank">
-                        esbuild v{version}
-                    </Text>
-                    <ActionIcon
-                        variant="outline"
-                        onClick={() => toggleColorScheme()}
-                        size="lg"
-                        sx={(theme) => ({
-                            backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
-                            color: theme.colorScheme === "dark" ? theme.colors.yellow[4] : theme.colors.blue[6],
-                        })}
-                    >
-                        {colorScheme === "dark" ? <IconSun size={18} /> : <IconMoonStars size={18} />}
-                    </ActionIcon>
-                </Group>
-            </div>
-            <App />
-        </MantineProvider>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{ colorScheme }}
+            >
+                <App />
+            </MantineProvider>
+        </ColorSchemeProvider>
     );
 }
