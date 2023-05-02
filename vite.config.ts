44419a9a1c7d24788467a67ad718bef8863dd7d5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-05-02 13:13:38 -0700

    Copy service worker file into output in vite instead

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,29 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
 import { comlink } from "vite-plugin-comlink";
+import Inspect from "vite-plugin-inspect";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "./",
     plugins: [
+        Inspect(),
         comlink(),
         react(),
         splitVendorChunkPlugin(),
         nodePolyfills({
             // Whether to polyfill `node:` protocol imports.
             protocolImports: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "node_modules/coi-serviceworker/coi-serviceworker.min.js",
+                    dest: ".",
+                },
+            ],
         }),
     ],
     worker: {
