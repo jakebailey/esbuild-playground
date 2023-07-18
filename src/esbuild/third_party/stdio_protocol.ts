e6a0539fc8ec6bb32e94190dcad47d7ed62b52ca
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2023-07-17 22:14:33 -0700

    Update stuff

--- src/esbuild/third_party/stdio_protocol.ts
+++ src/esbuild/third_party/stdio_protocol.ts
@@ -1,4 +1,4 @@
-const encoder = new TextEncoder()
-const decoder = new TextDecoder()
-export const encodeUTF8: (text: string) => Uint8Array = text => encoder.encode(text)
-export const decodeUTF8: (bytes: Uint8Array) => string = bytes => decoder.decode(bytes)
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+export const encodeUTF8: (text: string) => Uint8Array = (text) => encoder.encode(text);
+export const decodeUTF8: (bytes: Uint8Array) => string = (bytes) => decoder.decode(bytes);
